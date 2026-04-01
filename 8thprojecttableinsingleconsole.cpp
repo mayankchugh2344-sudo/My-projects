4ff9fa0687d458052cdce41604215f637b1aa4e8
Mayank Chugh <mayankchugh2344@gmail.com>
2026-04-01 19:31:21 +0530

    8th commit

---
+++ 8thprojecttableinsingleconsole.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int i,n;
+  cout<<"please enter no n"<<endl;
+cin>>n;
+//here the loop starts
+for(int i=1;i<=10;i++){cout<<i*n<<endl;}
+    return 0;
+}
