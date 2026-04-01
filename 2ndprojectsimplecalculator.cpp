00ff430ad6bb626a2e692cec3b8632660dc16627
Mayank Chugh <mayankchugh2344@gmail.com>
2026-04-01 15:32:07 +0530

    3rd commit

---
+++ 2ndprojectsimplecalculator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int a,b,c;
+
+    cout <<"please write the numbers you want to add" << endl;
+cin>>a>>b;
+c=a+b;
+cout<<"sum is"<<c;
+    return 0;
+}
