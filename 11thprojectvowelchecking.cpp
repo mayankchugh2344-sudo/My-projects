b6c7b6f49f66217520b03ee57d2c8fc3c753952a
Mayank Chugh <mayankchugh2344@gmail.com>
2026-04-01 20:04:49 +0530

    11th ccommit

---
+++ 11thprojectvowelchecking.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+char ch;
+cout<<"please enter alphabet";
+cin>>ch;
+//vowel checking
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')(cout<<"vowel"<<endl);
+else(cout<<"consonant"<<endl);
+    return 0;
+}
