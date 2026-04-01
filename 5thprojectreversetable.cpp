2c79836fdedc5d36c0b21f99ffeaefad5389908b
Mayank Chugh <mayankchugh2344@gmail.com>
2026-04-01 16:01:47 +0530

    6th commit

---
+++ 5thprojectreversetable.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // we will use for loop
+int i,n; 
+cout<<"please write the number"<<endl;
+cin>>n;
+for(int i=10;i>=1;i--){
+    cout<<i*n<<endl;
+}
+    return 0;
+}
