6cd8f8219cdeb83c3b0347ff22da9d2a0178c8ea
Mayank Chugh <mayankchugh2344@gmail.com>
2026-04-01 16:20:48 +0530

    my 7th commit

---
+++ 6thprojectoddeven.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+cout<<"please enter the number you want to check";
+    cin>>n;
+    if(n%2==0){cout<<"this no is even"<<endl;}
+        if(n%2!=0){cout<<"this no is even"<<endl;}
+    return 0;
+}
