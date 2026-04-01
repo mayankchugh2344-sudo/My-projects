ced21d51c7fa87e8abd59145b9c1de81db078c0d
Mayank Chugh <mayankchugh2344@gmail.com>
2026-04-01 19:10:53 +0530

    6th commit

--- 6thprojectoddeven.cpp
+++ 6thprojectoddeven.cpp
@@ -6,6 +6,6 @@ int main() {
 cout<<"please enter the number you want to check";
     cin>>n;
     if(n%2==0){cout<<"this no is even"<<endl;}
-        if(n%2!=0){cout<<"this no is even"<<endl;}
+        if(n%2!=0){cout<<"this no is odd"<<endl;}
     return 0;
 }
