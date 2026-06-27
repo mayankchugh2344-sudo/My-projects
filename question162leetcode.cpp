66505de388dc9ffbea6b8cce55994601f8b2bd06
Mayank Chugh <mayankchugh2344@gmail.com>
2026-06-27 13:52:44 +0530

    16thcommit

---
+++ question162leetcode.cpp
@@ -0,0 +1,36 @@
+
+
+    
+ #include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements of the array: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int max = arr[0];
+
+    for(int i = 1; i < n; i++)
+    {
+        if(arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    cout << "The maximum element is: " << max << endl;
+
+    return 0;
+}
