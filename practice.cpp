a1ded33ad2020a6c5d9988f5a77674c03cd21bba
Mayank Chugh <mayankchugh2344@gmail.com>
2026-09-03 21:39:25 +0530

    19thcommit

---
+++ practice.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+
+    int evalRPN(vector<string>& tokens) {
+
+        stack<int> st;
+
+        for (int i = 0; i < tokens.size(); i++) {
+
+            // If token is an operator
+            if (tokens[i] == "+" || tokens[i] == "-" ||
+                tokens[i] == "*" || tokens[i] == "/") {
+
+                int b = st.top();
+                st.pop();
+
+                int a = st.top();
+                st.pop();
+
+                if (tokens[i] == "+") {
+                    st.push(a + b);
+                }
+                else if (tokens[i] == "-") {
+                    st.push(a - b);
+                }
+                else if (tokens[i] == "*") {
+                    st.push(a * b);
+                }
+                else if (tokens[i] == "/") {
+                    st.push(a / b);
+                }
+            }
+
+            // If token is a number
+            else {
+                st.push(stoi(tokens[i]));
+            }
+        }
+
+        return st.top();
+    }
+};
+
+int main() {
+
+    int n;
+
+    cout << "Enter number of tokens: ";
+    cin >> n;
+
+    vector<string> tokens(n);
+
+    cout << "Enter tokens: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> tokens[i];
+    }
+
+    Solution obj;
+
+    int result = obj.evalRPN(tokens);
+
+    cout << "Result = " << result << endl;
+
+    return 0;
+}
